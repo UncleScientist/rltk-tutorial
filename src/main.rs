82e28661a07da130781fa8822a43552c6395b86f
Uncle Scientist <unclescientist@gmail.com>
2021-09-17 18:42:04 -0400

    Now the player can move using the arrow keys

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use rltk::{GameState, Rltk, RGB}; // , VirtualKeyCode};
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
 use specs::prelude::*;
-// use std::cmp::{max, min};
 use specs_derive::Component;
+use std::cmp::{max, min};
 
 #[derive(Component)]
 struct Position {
@@ -16,6 +16,9 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
 #[derive(Component)]
 struct LeftMover {}
 
@@ -25,6 +28,7 @@ struct State {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
+        player_input(self, ctx);
         self.run_systems();
 
         ctx.cls();
@@ -63,6 +67,29 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -72,6 +99,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -81,6 +109,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..10 {
