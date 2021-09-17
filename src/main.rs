bda692019e8f805ba174d116b46739ba46c150b3
Uncle Scientist <unclescientist@gmail.com>
2021-09-17 18:43:55 -0400

    slightly cleaner code

--- src/main.rs
+++ src/main.rs
@@ -78,15 +78,14 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
 }
 
 fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {}
-        Some(key) => match key {
+    if let Some(key) = ctx.key {
+        match key {
             VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
             VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
-        },
+        };
     }
 }
 
