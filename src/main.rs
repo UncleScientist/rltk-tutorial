01416e2fd32ea2759004c840741141d69e9f8e8b
Uncle Scientist <unclescientist@gmail.com>
2021-09-18 09:56:36 -0400

    creating and drawing a map

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
 
+// ------------------------------------------------------------
+// Component Section
+
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -22,6 +25,82 @@ struct Player {}
 #[derive(Component)]
 struct LeftMover {}
 
+// ------------------------------------------------------------
+// Map Section
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y * 80 + x) as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    let player_pos = xy_idx(40, 25);
+
+    for _i in 0..400 {
+        let (x, y) = (rng.roll_dice(1, 79), rng.roll_dice(1, 49));
+        let idx = xy_idx(x, y);
+        if idx != player_pos {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+// ------------------------------------------------------------
+// World state section
+
 struct State {
     ecs: World,
 }
@@ -32,6 +111,9 @@ impl GameState for State {
         self.run_systems();
 
         ctx.cls();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -99,6 +181,8 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
