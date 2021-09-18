0cb92ddee874b6db673535aa1e3495f33dc02cd0
Uncle Scientist <unclescientist@gmail.com>
2021-09-18 09:38:39 -0400

    player storage as read-only

--- src/main.rs
+++ src/main.rs
@@ -68,10 +68,10 @@ impl<'a> System<'a> for LeftWalker {
 }
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let players = ecs.read_storage::<Player>();
     let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
+    for (_player, pos) in (&players, &mut positions).join() {
         pos.x = min(79, max(0, pos.x + delta_x));
         pos.y = min(49, max(0, pos.y + delta_y));
     }
