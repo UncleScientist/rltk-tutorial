87366c211b7b04fc4759d03124ea39cf41bf9a27
Uncle Scientist <unclescientist@gmail.com>
2021-09-19 09:20:06 -0400

    draw horizontal tunnel between rooms

--- src/lib/map.rs
+++ src/lib/map.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use rltk::{RandomNumberGenerator, Rltk, RGB};
+use std::cmp::{max, min};
 
 // ------------------------------------------------------------
 // Map Section
@@ -12,6 +13,24 @@ pub enum TileType {
 
 pub fn xy_idx(x: i32, y: i32) -> usize {
     (y * 80 + x) as usize
+}
+
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+    for x in min(x1, x2)..=max(x1, x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+    for y in min(y1, y2)..=max(y1, y2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
 }
 
 fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
@@ -30,6 +49,7 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
 
     apply_room_to_map(&room1, &mut map);
     apply_room_to_map(&room2, &mut map);
+    apply_horizontal_tunnel(&mut map, 25, 40, 23);
 
     map
 }
