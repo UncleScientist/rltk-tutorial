fefa1b6affd2737040449666f5515f688756c1ac
Uncle Scientist <unclescientist@gmail.com>
2021-09-19 09:26:47 -0400

    creating a bunch of random rooms around the map

--- src/lib/map.rs
+++ src/lib/map.rs
@@ -15,6 +15,7 @@ pub fn xy_idx(x: i32, y: i32) -> usize {
     (y * 80 + x) as usize
 }
 
+/*
 fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
     for x in min(x1, x2)..=max(x1, x2) {
         let idx = xy_idx(x, y);
@@ -32,6 +33,7 @@ fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
         }
     }
 }
+*/
 
 fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
     for y in room.y1 + 1..=room.y2 {
@@ -44,12 +46,31 @@ fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
     let mut map = vec![TileType::Wall; 80 * 50];
 
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(35, 15, 10, 15);
+    let mut rooms: Vec<Rect> = Vec::new();
+    const MAX_ROOMS: i32 = 30;
+    const MIN_SIZE: i32 = 6;
+    const MAX_SIZE: i32 = 10;
 
-    apply_room_to_map(&room1, &mut map);
-    apply_room_to_map(&room2, &mut map);
-    apply_horizontal_tunnel(&mut map, 25, 40, 23);
+    let mut rng = RandomNumberGenerator::new();
+
+    for _ in 0..MAX_ROOMS {
+        let w = rng.range(MIN_SIZE, MAX_SIZE);
+        let h = rng.range(MIN_SIZE, MAX_SIZE);
+        let x = rng.roll_dice(1, 80 - w - 1) - 1;
+        let y = rng.roll_dice(1, 50 - h - 1) - 1;
+        let new_room = Rect::new(x, y, w, h);
+        let mut ok = true;
+        for other_room in rooms.iter() {
+            if new_room.intersect(other_room) {
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            apply_room_to_map(&new_room, &mut map);
+            rooms.push(new_room);
+        }
+    }
 
     map
 }
--- src/lib/rect.rs
+++ src/lib/rect.rs
@@ -14,4 +14,8 @@ impl Rect {
             y2: y + h,
         }
     }
+
+    pub fn intersect(&self, other: &Rect) -> bool {
+        self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.y2 >= other.y1
+    }
 }
