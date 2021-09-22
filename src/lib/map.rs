ac2a36b1fd07ada070a217a2574dcb2e75ae243b
Uncle Scientist <unclescientist@gmail.com>
2021-09-22 07:36:24 -0400

    grey out non-visible but known areas

--- src/lib/map.rs
+++ src/lib/map.rs
@@ -17,6 +17,7 @@ pub struct Map {
     pub width: i32,
     pub height: i32,
     pub revealed_tiles: Vec<bool>,
+    pub visible_tiles: Vec<bool>,
 }
 
 impl Map {
@@ -62,6 +63,7 @@ impl Map {
             width: 80,
             height: 50,
             revealed_tiles: vec![false; 80 * 50],
+            visible_tiles: vec![false; 80 * 50],
         };
 
         const MAX_ROOMS: i32 = 30;
@@ -132,14 +134,14 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
 
     for (idx, tile) in map.tiles.iter().enumerate() {
         if map.revealed_tiles[idx] {
-            match tile {
-                TileType::Floor => {
-                    ctx.set(x, y, grey, black, floor);
-                }
-                TileType::Wall => {
-                    ctx.set(x, y, green, black, wall);
-                }
+            let (glyph, mut fg) = match tile {
+                TileType::Floor => (floor, grey),
+                TileType::Wall => (wall, green),
+            };
+            if !map.visible_tiles[idx] {
+                fg = fg.to_greyscale();
             }
+            ctx.set(x, y, fg, black, glyph);
         }
 
         x += 1;
--- src/lib/visibility_system.rs
+++ src/lib/visibility_system.rs
@@ -20,16 +20,21 @@ impl<'a> System<'a> for VisibilitySystem {
             if viewshed.dirty {
                 viewshed.dirty = false;
                 viewshed.visible_tiles.clear();
-                viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+                viewshed.visible_tiles =
+                    field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
                 viewshed
                     .visible_tiles
                     .retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
 
                 // If this is the player, reveal what they can see
                 if player.get(ent).is_some() {
+                    for t in map.visible_tiles.iter_mut() {
+                        *t = false;
+                    }
                     for vis in viewshed.visible_tiles.iter() {
                         let idx = map.xy_idx(vis.x, vis.y);
                         map.revealed_tiles[idx] = true;
+                        map.visible_tiles[idx] = true;
                     }
                 }
             }
