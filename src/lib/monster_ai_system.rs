6e8a8d5d05eb9f8b8ff8387377e5926e07de7118
Uncle Scientist <unclescientist@gmail.com>
2021-10-01 08:16:49 -0400

    monsters can now follow you and chase you

--- src/lib/monster_ai_system.rs
+++ src/lib/monster_ai_system.rs
@@ -1,22 +1,40 @@
-use crate::{Monster, Name, Viewshed};
+use crate::{Map, Monster, Name, Position, Viewshed};
 use rltk::{console, Point};
 use specs::prelude::*;
 
 pub struct MonsterAI {}
 
-impl<'a> System<'a> for MonsterAI {
-    type SystemData = (
-        ReadExpect<'a, Point>,
-        ReadStorage<'a, Viewshed>,
-        ReadStorage<'a, Monster>,
-        ReadStorage<'a, Name>,
-    );
-    fn run(&mut self, data: Self::SystemData) {
-        let (player_pos, viewshed, monster, name) = data;
+type MonsterAIData<'a> = (
+    ReadExpect<'a, Map>,
+    ReadExpect<'a, Point>,
+    WriteStorage<'a, Viewshed>,
+    ReadStorage<'a, Monster>,
+    ReadStorage<'a, Name>,
+    WriteStorage<'a, Position>,
+);
 
-        for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
+impl<'a> System<'a> for MonsterAI {
+    type SystemData = MonsterAIData<'a>;
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (map, player_pos, mut viewshed, monster, name, mut position) = data;
+
+        for (mut viewshed, _monster, name, mut pos) in
+            (&mut viewshed, &monster, &name, &mut position).join()
+        {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(&format!("{} shouts insults", name.name));
+                let path = rltk::a_star_search(
+                    map.xy_idx(pos.x, pos.y) as i32,
+                    map.xy_idx(player_pos.x, player_pos.y) as i32,
+                    &*map,
+                );
+
+                if path.success && path.steps.len() > 1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
