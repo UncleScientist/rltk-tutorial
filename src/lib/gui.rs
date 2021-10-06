b3231757813381574b21cc7b656a8de3237ee22f
Uncle Scientist <unclescientist@gmail.com>
2021-10-06 07:25:21 -0400

    draw health bar for player

--- src/lib/gui.rs
+++ src/lib/gui.rs
@@ -1,7 +1,8 @@
-use rltk::{Rltk, RGB};
+use crate::{CombatStats, Player};
+use rltk::{Rltk, BLACK, RED, RGB, YELLOW};
 use specs::prelude::*;
 
-pub fn draw_ui(_ecs: &World, ctx: &mut Rltk) {
+pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
     ctx.draw_box(
         0,
         43,
@@ -10,4 +11,21 @@ pub fn draw_ui(_ecs: &World, ctx: &mut Rltk) {
         RGB::named(rltk::WHITE),
         RGB::named(rltk::BLACK),
     );
+
+    let combat_stats = ecs.read_storage::<CombatStats>();
+    let players = ecs.read_storage::<Player>();
+
+    for (_player, stats) in (&players, &combat_stats).join() {
+        let health = format!(" HP: {} / {} ", stats.hp, stats.max_hp);
+        ctx.print_color(12, 43, RGB::named(YELLOW), RGB::named(BLACK), &health);
+        ctx.draw_bar_horizontal(
+            28,
+            43,
+            51,
+            stats.hp,
+            stats.max_hp,
+            RGB::named(RED),
+            RGB::named(BLACK),
+        );
+    }
 }
