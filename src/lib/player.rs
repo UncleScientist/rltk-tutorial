19bd079a75a1373d9dadb41c9950971ee7ab334d
Uncle Scientist <unclescientist@gmail.com>
2021-11-02 09:51:40 -0400

    player can rest and optionally heal if no mobs nearby

--- src/lib/player.rs
+++ src/lib/player.rs
@@ -74,6 +74,7 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
             Numpad7 | Y => try_move_player(-1, -1, &mut gs.ecs),
             Numpad3 | N => try_move_player(1, 1, &mut gs.ecs),
             Numpad1 | B => try_move_player(-1, 1, &mut gs.ecs),
+            Numpad5 | Space => return skip_turn(&mut gs.ecs),
             Period => {
                 if try_next_level(&mut gs.ecs) {
                     return RunState::NextLevel;
@@ -91,6 +92,34 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     } else {
         RunState::AwaitingInput
     }
+}
+
+fn skip_turn(ecs: &mut World) -> RunState {
+    let player_entity = ecs.fetch::<Entity>();
+    let viewshed_components = ecs.read_storage::<Viewshed>();
+    let monsters = ecs.read_storage::<Monster>();
+
+    let worldmap_resource = ecs.fetch::<Map>();
+
+    let mut can_heal = true;
+    let viewshed = viewshed_components.get(*player_entity).unwrap();
+    for tile in viewshed.visible_tiles.iter() {
+        let idx = worldmap_resource.xy_idx(tile.x, tile.y);
+        for entity_id in worldmap_resource.tile_content[idx].iter() {
+            if monsters.get(*entity_id).is_some() {
+                can_heal = false;
+                break;
+            }
+        }
+    }
+
+    if can_heal {
+        let mut health_components = ecs.write_storage::<CombatStats>();
+        let player_hp = health_components.get_mut(*player_entity).unwrap();
+        player_hp.hp = i32::min(player_hp.hp + 1, player_hp.max_hp);
+    }
+
+    RunState::PlayerTurn
 }
 
 fn get_item(ecs: &mut World) {
