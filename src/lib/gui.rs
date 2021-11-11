62213169fd9b6d4e06aa50ce3f3e20ac4d253f64
Uncle Scientist <unclescientist@gmail.com>
2021-11-11 12:29:08 -0500

    only allow ESC key for restarting game

--- src/lib/gui.rs
+++ src/lib/gui.rs
@@ -392,11 +392,11 @@ pub fn game_over(ctx: &mut Rltk) -> GameOverResult {
         20,
         RGB::named(rltk::MAGENTA),
         RGB::named(rltk::BLACK),
-        "Press any key to return to the menu.",
+        "Press ESC to return to the menu.",
     );
 
     match ctx.key {
-        None => GameOverResult::NoSelection,
-        Some(_) => GameOverResult::QuitToMenu,
+        Some(VirtualKeyCode::Escape) => GameOverResult::QuitToMenu,
+        _ => GameOverResult::NoSelection,
     }
 }
