1e54a56ec0607c1fcfb1cce0a1b90468fce87fc9
Uncle Scientist <unclescientist@gmail.com>
2021-11-11 12:25:53 -0500

    nicer wall tiles using cp437

--- src/lib/map.rs
+++ src/lib/map.rs
@@ -213,7 +213,6 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
     let green = RGB::from_f32(0., 1., 0.);
     let cyan = RGB::from_f32(0., 1., 1.);
     let floor = rltk::to_cp437('.');
-    let wall = rltk::to_cp437('#');
     let down_stairs = rltk::to_cp437('>');
 
     let map = ecs.fetch::<Map>();
@@ -225,7 +224,7 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
         if map.revealed_tiles[idx] {
             let (glyph, mut fg) = match tile {
                 TileType::Floor => (floor, grey),
-                TileType::Wall => (wall, green),
+                TileType::Wall => (wall_glyph(&*map, x, y), green),
                 TileType::DownStairs => (down_stairs, cyan),
             };
             if !map.visible_tiles[idx] {
@@ -240,4 +239,50 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
             y += 1;
         }
     }
+}
+
+fn wall_glyph(map: &Map, x: i32, y: i32) -> rltk::FontCharType {
+    if x < 1 || x > map.width - 2 || y < 1 || y > map.height - 2 {
+        return 35;
+    }
+
+    let mut mask: u8 = 0;
+
+    if is_revealed_and_wall(map, x, y - 1) {
+        mask += 1
+    };
+    if is_revealed_and_wall(map, x, y + 1) {
+        mask += 2
+    };
+    if is_revealed_and_wall(map, x - 1, y) {
+        mask += 4
+    };
+    if is_revealed_and_wall(map, x + 1, y) {
+        mask += 8
+    };
+
+    match mask {
+        0 => 9,
+        1 => 186,
+        2 => 186,
+        3 => 186,
+        4 => 205,
+        5 => 188,
+        6 => 187,
+        7 => 185,
+        8 => 205,
+        9 => 200,
+        10 => 201,
+        11 => 204,
+        12 => 205,
+        13 => 202,
+        14 => 203,
+        15 => 206,
+        _ => 35,
+    }
+}
+
+fn is_revealed_and_wall(map: &Map, x: i32, y: i32) -> bool {
+    let idx = map.xy_idx(x, y);
+    map.tiles[idx] == TileType::Wall && map.revealed_tiles[idx]
 }
