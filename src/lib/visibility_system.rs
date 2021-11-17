ebe80bc4191a8f825554b3f90b8510d37e7a1111
Uncle Scientist <unclescientist@gmail.com>
2021-11-17 07:28:56 -0500

    1/24 chance of spotting a bear trap on every step

--- src/lib/visibility_system.rs
+++ src/lib/visibility_system.rs
@@ -1,20 +1,27 @@
-use crate::{Map, Player, Position, Viewshed};
+use crate::{GameLog, Hidden, Map, Name, Player, Position, Viewshed};
 use rltk::{field_of_view, Point};
 use specs::prelude::*;
 
 pub struct VisibilitySystem {}
 
+type VisibilityData<'a> = (
+    WriteExpect<'a, Map>,
+    Entities<'a>,
+    WriteStorage<'a, Viewshed>,
+    WriteStorage<'a, Position>,
+    ReadStorage<'a, Player>,
+    WriteStorage<'a, Hidden>,
+    WriteExpect<'a, rltk::RandomNumberGenerator>,
+    WriteExpect<'a, GameLog>,
+    ReadStorage<'a, Name>,
+);
+
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = (
-        WriteExpect<'a, Map>,
-        Entities<'a>,
-        WriteStorage<'a, Viewshed>,
-        WriteStorage<'a, Position>,
-        ReadStorage<'a, Player>,
-    );
+    type SystemData = VisibilityData<'a>;
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut map, entities, mut viewshed, pos, player) = data;
+        let (mut map, entities, mut viewshed, pos, player, mut hidden, mut rng, mut log, names) =
+            data;
 
         for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
             if viewshed.dirty {
@@ -35,6 +42,16 @@ impl<'a> System<'a> for VisibilitySystem {
                         let idx = map.xy_idx(vis.x, vis.y);
                         map.revealed_tiles[idx] = true;
                         map.visible_tiles[idx] = true;
+
+                        // Chance to reveal hidden things
+                        for e in map.tile_content[idx].iter() {
+                            if hidden.get(*e).is_some() && rng.roll_dice(1, 24) == 1 {
+                                if let Some(name) = names.get(*e) {
+                                    log.entries.push(format!("You spotted a {}.", &name.name));
+                                }
+                                hidden.remove(*e);
+                            }
+                        }
                     }
                 }
             }
