be5e93ebac238ce50d270754b0cf65d393ed0775
Uncle Scientist <unclescientist@gmail.com>
2021-11-22 08:04:52 -0500

    add bsp-interior map style

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -10,6 +10,9 @@ use simple_map::SimpleMapBuilder;
 mod bsp_dungeon;
 use bsp_dungeon::BspDungeonBuilder;
 
+mod bsp_interior;
+use bsp_interior::BspInteriorBuilder;
+
 pub trait MapBuilder {
     fn build_map(&mut self);
     fn spawn_entities(&mut self, ecs: &mut World);
@@ -21,9 +24,10 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 2);
+    let builder = rng.roll_dice(1, 3);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        2 => Box::new(BspInteriorBuilder::new(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
