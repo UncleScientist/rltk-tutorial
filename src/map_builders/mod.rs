c6fea39a6c967d01d740f2e1a2b3067b194d07be
Uncle Scientist <unclescientist@gmail.com>
2021-11-22 07:31:55 -0500

    added a new BSP-style map builder and random choice of map

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -7,6 +7,9 @@ use common::*;
 mod simple_map;
 use simple_map::SimpleMapBuilder;
 
+mod bsp_dungeon;
+use bsp_dungeon::BspDungeonBuilder;
+
 pub trait MapBuilder {
     fn build_map(&mut self);
     fn spawn_entities(&mut self, ecs: &mut World);
@@ -17,5 +20,10 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    Box::new(SimpleMapBuilder::new(new_depth))
+    let mut rng = rltk::RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 2);
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    }
 }
