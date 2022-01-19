bd547478618af86f87c3f6ae2c8d2bae6e2b6f6f
Uncle Scientist <unclescientist@gmail.com>
2022-01-19 08:34:34 -0500

    added a yellow brick road

--- src/map_builders/forest.rs
+++ src/map_builders/forest.rs
@@ -1,8 +1,9 @@
 use super::{
-    AreaStartingPosition, BuilderChain, CellularAutomataBuilder, CullUnreachable, DistantExit,
-    VoronoiSpawning, XStart, YStart,
+    AreaStartingPosition, BuilderChain, BuilderMap, CellularAutomataBuilder, CullUnreachable,
+    MetaMapBuilder, TileType, VoronoiSpawning, XStart, YStart,
 };
-use rltk::RandomNumberGenerator;
+use crate::map;
+use rltk::{a_star_search, DistanceAlg, Point, RandomNumberGenerator};
 
 pub fn forest_builder(
     new_depth: i32,
@@ -18,7 +19,82 @@ pub fn forest_builder(
 
     // Set up an exit and spawn mobs
     chain.with(VoronoiSpawning::new());
-    chain.with(DistantExit::new());
+    chain.with(YellowBrickRoad::new());
 
     chain
+}
+
+pub struct YellowBrickRoad {}
+
+impl MetaMapBuilder for YellowBrickRoad {
+    fn build_map(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
+        self.build(rng, build_data);
+    }
+}
+
+impl YellowBrickRoad {
+    pub fn new() -> Box<YellowBrickRoad> {
+        Box::new(YellowBrickRoad {})
+    }
+
+    fn find_exit(&self, build_data: &mut BuilderMap, seed_x: i32, seed_y: i32) -> (i32, i32) {
+        let mut available_floors: Vec<(usize, f32)> = Vec::new();
+        for (idx, tiletype) in build_data.map.tiles.iter().enumerate() {
+            if map::tile_walkable(*tiletype) {
+                available_floors.push((
+                    idx,
+                    DistanceAlg::PythagorasSquared.distance2d(
+                        Point::new(
+                            idx as i32 % build_data.map.width,
+                            idx as i32 / build_data.map.width,
+                        ),
+                        Point::new(seed_x, seed_y),
+                    ),
+                ));
+            }
+        }
+        if available_floors.is_empty() {
+            panic!("No valid floors to start on");
+        }
+
+        available_floors.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+        let end_x = available_floors[0].0 as i32 % build_data.map.width;
+        let end_y = available_floors[0].0 as i32 / build_data.map.width;
+        (end_x, end_y)
+    }
+
+    fn paint_road(&self, build_data: &mut BuilderMap, x: i32, y: i32) {
+        if x < 1 || x > build_data.map.width - 2 || y < 1 || y > build_data.map.height - 2 {
+            return;
+        }
+        let idx = build_data.map.xy_idx(x, y);
+        if build_data.map.tiles[idx] != TileType::DownStairs {
+            build_data.map.tiles[idx] = TileType::Road;
+        }
+    }
+
+    fn build(&mut self, _rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
+        let starting_pos = build_data.starting_position.as_ref().unwrap().clone();
+        let start_idx = build_data.map.xy_idx(starting_pos.x, starting_pos.y);
+
+        let (end_x, end_y) = self.find_exit(
+            build_data,
+            build_data.map.width - 2,
+            build_data.map.height / 2,
+        );
+        let end_idx = build_data.map.xy_idx(end_x, end_y);
+        build_data.map.tiles[end_idx] = TileType::DownStairs;
+
+        let path = a_star_search(start_idx, end_idx, &build_data.map);
+        for idx in path.steps.iter() {
+            let x = *idx as i32 % build_data.map.width;
+            let y = *idx as i32 / build_data.map.width;
+            self.paint_road(build_data, x, y);
+            self.paint_road(build_data, x - 1, y);
+            self.paint_road(build_data, x + 1, y);
+            self.paint_road(build_data, x, y - 1);
+            self.paint_road(build_data, x, y + 1);
+        }
+        build_data.take_snapshot();
+    }
 }
