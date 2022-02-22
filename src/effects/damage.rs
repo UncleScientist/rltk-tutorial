10eb0f5570dc1a1f967ee85e1ffee3d624d701ee
Uncle Scientist <unclescientist@gmail.com>
2022-02-22 08:31:33 -0500

    added bloodstains and fixed recursive mutex bug

--- src/effects/damage.rs
+++ src/effects/damage.rs
@@ -1,14 +1,23 @@
-use specs::prelude::*;
 use super::*;
 use crate::components::Pools;
+use crate::map::Map;
+use targetting::entity_position;
 
 pub fn inflict_damage(ecs: &mut World, damage: &EffectSpawner, target: Entity) {
     let mut pools = ecs.write_storage::<Pools>();
     if let Some(pool) = pools.get_mut(target) {
         if !pool.god_mode {
-            if let EffectType::Damage{amount} = damage.effect_type {
+            if let EffectType::Damage { amount } = damage.effect_type {
                 pool.hit_points.current -= amount;
+                if let Some(tile_idx) = entity_position(ecs, target) {
+                    add_effect(None, EffectType::Bloodstain, Targets::Tile { tile_idx });
+                }
             }
         }
     }
+}
+
+pub fn bloodstain(ecs: &mut World, tile_idx: i32) {
+    let mut map = ecs.fetch_mut::<Map>();
+    map.bloodstains.insert(tile_idx as usize);
 }
--- src/effects/mod.rs
+++ src/effects/mod.rs
@@ -1,12 +1,15 @@
 use std::collections::VecDeque;
 use std::sync::Mutex;
 
-mod damage;
-use damage::inflict_damage;
-
 use specs::prelude::*;
 
 use lazy_static::lazy_static;
+
+mod damage;
+pub use damage::*;
+
+mod targetting;
+pub use targetting::*;
 
 lazy_static! {
     pub static ref EFFECT_QUEUE: Mutex<VecDeque<EffectSpawner>> = Mutex::new(VecDeque::new());
@@ -14,6 +17,7 @@ lazy_static! {
 
 pub enum EffectType {
     Damage { amount: i32 },
+    Bloodstain,
 }
 
 #[derive(Clone)]
@@ -39,18 +43,21 @@ pub fn add_effect(creator: Option<Entity>, effect_type: EffectType, targets: Tar
 }
 
 pub fn run_effects_queue(ecs: &mut World) {
+    /*
+     * Cannot do it this way because the lock is being held inside the while
+     *
     while let Some(effect) = EFFECT_QUEUE.lock().unwrap().pop_front() {
         target_applicator(ecs, &effect);
     }
-
-    /* loop {
-        let effect : Option<EffectSpawner> = EFFECT_QUEUE.lock().unwrap().pop_front();
+    */
+    loop {
+        let effect = EFFECT_QUEUE.lock().unwrap().pop_front();
         if let Some(effect) = effect {
-            // target_applicator(ecs, &effect);
+            target_applicator(ecs, &effect);
         } else {
             break;
         }
-    } */
+    }
 }
 
 fn target_applicator(ecs: &mut World, effect: &EffectSpawner) {
@@ -69,6 +76,7 @@ fn target_applicator(ecs: &mut World, effect: &EffectSpawner) {
 fn tile_effect_hits_entities(effect: &EffectType) -> bool {
     match effect {
         EffectType::Damage { .. } => true,
+        _ => false,
     }
 }
 
@@ -79,9 +87,15 @@ fn affect_tile(ecs: &mut World, effect: &EffectSpawner, tile_idx: i32) {
         });
     }
 
-    // TODO: run the effect
+    match &effect.effect_type {
+        EffectType::Bloodstain => damage::bloodstain(ecs, tile_idx),
+        _ => {}
+    }
 }
 
 fn affect_entity(ecs: &mut World, effect: &EffectSpawner, target: Entity) {
-    inflict_damage(ecs, effect, target);
+    match &effect.effect_type {
+        EffectType::Damage { .. } => damage::inflict_damage(ecs, effect, target),
+        _ => {}
+    }
 }
