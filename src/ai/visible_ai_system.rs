d74f708af5a969976d8edc4f94b6b90a1bf3cb28
Uncle Scientist <unclescientist@gmail.com>
2022-03-25 07:32:51 -0400

    badit archers can now shoot the player

--- src/ai/visible_ai_system.rs
+++ src/ai/visible_ai_system.rs
@@ -1,8 +1,8 @@
 use rltk::RandomNumberGenerator;
 
 use crate::{
-    raws::Reaction, Chasing, Faction, Map, MyTurn, Name, Position, SpecialAbilities, SpellTemplate,
-    Viewshed, WantsToApproach, WantsToCastSpell, WantsToFlee,
+    raws::Reaction, Chasing, Equipped, Faction, Map, MyTurn, Name, Position, SpecialAbilities,
+    SpellTemplate, Viewshed, WantsToApproach, WantsToCastSpell, WantsToFlee, WantsToShoot, Weapon,
 };
 use specs::prelude::*;
 
@@ -24,6 +24,9 @@ type VisibleData<'a> = (
     WriteStorage<'a, WantsToCastSpell>,
     ReadStorage<'a, Name>,
     ReadStorage<'a, SpellTemplate>,
+    ReadStorage<'a, Equipped>,
+    ReadStorage<'a, Weapon>,
+    WriteStorage<'a, WantsToShoot>,
 );
 
 impl<'a> System<'a> for VisibleAI {
@@ -46,6 +49,9 @@ impl<'a> System<'a> for VisibleAI {
             mut casting,
             names,
             spells,
+            equipped,
+            weapons,
+            mut wants_shoot,
         ) = data;
 
         for (entity, _turn, my_faction, pos, viewshed) in
@@ -67,15 +73,14 @@ impl<'a> System<'a> for VisibleAI {
                 for reaction in reactions.iter() {
                     match reaction.1 {
                         Reaction::Attack => {
+                            let range = rltk::DistanceAlg::Pythagoras.distance2d(
+                                rltk::Point::new(pos.x, pos.y),
+                                rltk::Point::new(
+                                    reaction.0 as i32 % map.width,
+                                    reaction.0 as i32 / map.width,
+                                ),
+                            );
                             if let Some(abilities) = abilities.get(entity) {
-                                let range = rltk::DistanceAlg::Pythagoras.distance2d(
-                                    rltk::Point::new(pos.x, pos.y),
-                                    rltk::Point::new(
-                                        reaction.0 as i32 % map.width,
-                                        reaction.0 as i32 / map.width,
-                                    ),
-                                );
-
                                 for ability in abilities.abilities.iter() {
                                     if range >= ability.min_range
                                         && range <= ability.range
@@ -101,6 +106,28 @@ impl<'a> System<'a> for VisibleAI {
                                             )
                                             .expect("Unable to insert");
                                         done = true;
+                                    }
+                                }
+                            }
+
+                            if !done {
+                                for (weapon, equip) in (&weapons, &equipped).join() {
+                                    if let Some(wrange) = weapon.range {
+                                        if equip.owner == entity {
+                                            rltk::console::log(format!(
+                                                "Onwer found. Ranges: {wrange}/{range}"
+                                            ));
+                                            if wrange >= range as i32 {
+                                                rltk::console::log("Inserting shoot");
+                                                wants_shoot
+                                                    .insert(
+                                                        entity,
+                                                        WantsToShoot { target: reaction.2 },
+                                                    )
+                                                    .expect("Insert fail");
+                                                done = true;
+                                            }
+                                        }
                                     }
                                 }
                             }
